afb6f7f6c80f221d6cc61d61e1a89fc839f6ed5a
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-27 18:49:54 +0530

    add styles and component dynamic

--- expense_tracker/src/components/expenseItem.js
+++ expense_tracker/src/components/expenseItem.js
@@ -1,10 +1,26 @@
+import "./expenseItem.css";
+
 function ExpenseItem() {
+  let itemName = "Food";
+  let date = new Date(20022, 10, 20);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  date = day + "/" + month + "/" + year;
+
+  let amount = 200;
+  let location = "Delhi";
   return (
     <div>
-      <h2>Expense Items</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movie Rs 200</p>
+      <h2 className="heading">Expense Items</h2>
+      <div className="itemsContainer">
+        <p className="items">
+          <span>{date}</span>
+          <span className="itemInfo">{itemName}</span>
+          <span className="itemInfo">{amount}</span>
+          <span className="itemInfo">{location}</span>
+        </p>
+      </div>
     </div>
   );
 }

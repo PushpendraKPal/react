9070ef1735f2736560f9a08855a9d77ecd27e45f
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-27 19:23:56 +0530

    used data

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -3,9 +3,82 @@ import "./App.css";
 import ExpenseItem from "./components/expenseItem";
 
 function App() {
+  const data = [
+    {
+      date: "2023-09-27",
+      name: "Groceries",
+      amount: 50.0,
+      location: "Local Supermarket",
+    },
+    {
+      date: "2023-09-26",
+      name: "Dinner",
+      amount: 30.0,
+      location: "Restaurant XYZ",
+    },
+    {
+      date: "2023-09-25",
+      name: "Gasoline",
+      amount: 40.0,
+      location: "Gas Station ABC",
+    },
+    {
+      date: "2023-09-24",
+      name: "Movie Tickets",
+      amount: 25.0,
+      location: "Cinema City",
+    },
+    {
+      date: "2023-09-23",
+      name: "Electronics",
+      amount: 300.0,
+      location: "Tech Store",
+    },
+    {
+      date: "2023-09-22",
+      name: "Lunch",
+      amount: 15.0,
+      location: "Cafeteria",
+    },
+    {
+      date: "2023-09-21",
+      name: "Home Rent",
+      amount: 1200.0,
+      location: "Landlord",
+    },
+    {
+      date: "2023-09-20",
+      name: "Clothing",
+      amount: 75.0,
+      location: "Fashion Store",
+    },
+    {
+      date: "2023-09-19",
+      name: "Utilities",
+      amount: 100.0,
+      location: "Utility Company",
+    },
+    {
+      date: "2023-09-18",
+      name: "Internet Bill",
+      amount: 50.0,
+      location: "Internet Provider",
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+      <h2>Expense Items</h2>
+      {data.map((ele) => {
+        return (
+          <ExpenseItem
+            name={ele.name}
+            date={ele.date}
+            amount={ele.amount}
+            location={ele.location}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
--- expense_tracker/src/components/expenseItem.js
+++ expense_tracker/src/components/expenseItem.js
@@ -1,26 +1,16 @@
 import "./expenseItem.css";
 
-function ExpenseItem() {
-  let itemName = "Food";
-  let date = new Date(20022, 10, 20);
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDate();
-  date = day + "/" + month + "/" + year;
+function ExpenseItem(props) {
+  const { date, name, amount, location } = props;
 
-  let amount = 200;
-  let location = "Delhi";
   return (
-    <div>
-      <h2 className="heading">Expense Items</h2>
-      <div className="itemsContainer">
-        <p className="items">
-          <span>{date}</span>
-          <span className="itemInfo">{itemName}</span>
-          <span className="itemInfo">{amount}</span>
-          <span className="itemInfo">{location}</span>
-        </p>
-      </div>
+    <div className="itemsContainer">
+      <p className="items">
+        <span>{date}</span>
+        <span className="itemInfo">{name}</span>
+        <span className="itemInfo">{amount * 10}</span>
+        <span className="itemInfo">{location}</span>
+      </p>
     </div>
   );
 }

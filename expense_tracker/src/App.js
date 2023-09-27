b2f57d97e8240bfa68c4b69e62927ee5275b6649
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-27 18:03:56 +0530

    add expense item component

---
+++ expense_tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/expenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;

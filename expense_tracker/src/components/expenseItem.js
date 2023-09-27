fb87b02925d80de430ab2423ebca626bc4062763
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-27 19:57:15 +0530

    passing data into child component

--- expense_tracker/src/components/expenseItem.js
+++ expense_tracker/src/components/expenseItem.js
@@ -1,16 +1,19 @@
 import "./expenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   const { date, name, amount, location } = props;
 
   return (
     <div className="itemsContainer">
-      <p className="items">
-        <span>{date}</span>
+      <div>
+        <ExpenseDate date={date}></ExpenseDate>
+      </div>
+      <div>
         <span className="itemInfo">{name}</span>
         <span className="itemInfo">{amount * 10}</span>
         <span className="itemInfo">{location}</span>
-      </p>
+      </div>
     </div>
   );
 }

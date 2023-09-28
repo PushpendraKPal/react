92e3c550f90a53986599522669a08de0eb26a936
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-28 19:46:53 +0530

    add delete button

--- expense_tracker/src/components/expenseItem.js
+++ expense_tracker/src/components/expenseItem.js
@@ -4,16 +4,26 @@ import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
   const { date, name, amount, location } = props;
 
+  const clickHandler = (event) => {
+    let parentDiv = event.currentTarget.parentNode;
+    console.log(parentDiv);
+    let grandparentNode = parentDiv.parentNode;
+    grandparentNode.removeChild(parentDiv);
+  };
+
   return (
     <div className="itemsContainer">
       <div>
-        <ExpenseDate date={date}></ExpenseDate>
+        <ExpenseDate date={date} key={date}></ExpenseDate>
       </div>
       <div>
         <span className="itemInfo">{name}</span>
         <span className="itemInfo">{amount * 10}</span>
         <span className="itemInfo">{location}</span>
       </div>
+      <button className="del" onClick={clickHandler}>
+        Delete
+      </button>
     </div>
   );
 };

6f8f130eacd359e15dd473a2878b8e811d692a12
PushpendraKPal <kumarpushp1992@gmail.com>
2023-09-29 08:33:47 +0530

    include useState

--- expense_tracker/src/components/expenseItem.js
+++ expense_tracker/src/components/expenseItem.js
@@ -1,14 +1,21 @@
 import "./expenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
   const { date, name, amount, location } = props;
 
-  const clickHandler = (event) => {
-    let parentDiv = event.currentTarget.parentNode;
-    console.log(parentDiv);
-    let grandparentNode = parentDiv.parentNode;
-    grandparentNode.removeChild(parentDiv);
+  // const clickHandler = (event) => {
+  //   let parentDiv = event.currentTarget.parentNode;
+  //   console.log(parentDiv);
+  //   let grandparentNode = parentDiv.parentNode;
+  //   grandparentNode.removeChild(parentDiv);
+  // };
+
+  const [price, newPrice] = useState(amount);
+
+  const clickHandler = () => {
+    newPrice(100);
   };
 
   return (
@@ -18,11 +25,11 @@ const ExpenseItem = (props) => {
       </div>
       <div>
         <span className="itemInfo">{name}</span>
-        <span className="itemInfo">{amount * 10}</span>
+        <span className="itemInfo">$ {price}</span>
         <span className="itemInfo">{location}</span>
       </div>
       <button className="del" onClick={clickHandler}>
-        Delete
+        Change Amount $100
       </button>
     </div>
   );

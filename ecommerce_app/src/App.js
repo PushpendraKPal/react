b35ee956f094f72c4de22886963e2c36c7d394cd
PushpendraKPal <kumarpushp1992@gmail.com>
2024-01-20 19:09:52 +0530

    applied lazyloading

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -3,17 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarList from "./components/NavbarList";
 import Footer from "./components/Footer";
 import Cart from "./components/Cart";
-import { useState } from "react";
+import { Suspense, lazy, useState } from "react";
 import Context from "./context/Context";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Store from "./pages/Store";
-import Contact from "./pages/Contact";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
+//import About from "./pages/About";
+//import Store from "./pages/Store";
+//import Contact from "./pages/Contact";
+//import Product from "./pages/Product";
+//import Login from "./pages/Login";
 import AuthCxt from "./context/AuthContext";
 import Logout from "./components/Logout";
+const LazyProduct = lazy(() => import("./pages/Product"));
+const LazyAbout = lazy(() => import("./pages/About"));
+const LazyContact = lazy(() => import("./pages/Contact"));
+const LazyLogin = lazy(() => import("./pages/Login"));
+const LazyStore = lazy(() => import("./pages/Store"));
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -27,14 +32,59 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route
             path="/store"
-            element={isLoggedIn ? <Store /> : <Login />}
+            element={
+              isLoggedIn ? (
+                <Suspense>
+                  <LazyStore />
+                </Suspense>
+              ) : (
+                <Suspense>
+                  <LazyLogin />
+                </Suspense>
+              )
+            }
             exact
           />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/store/:id" element={<Product />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
+          <Route
+            path="/about"
+            element={
+              <Suspense>
+                <LazyAbout />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <Suspense>
+                <LazyContact />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/store/:id"
+            element={
+              <Suspense>
+                <LazyProduct />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Suspense>
+                <LazyLogin />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/logout"
+            element={
+              <Suspense>
+                <LazyLogin />
+              </Suspense>
+            }
+          />
         </Routes>
 
         {/* <div className="header">The Generics</div>

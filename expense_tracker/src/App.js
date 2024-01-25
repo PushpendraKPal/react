a022c7ff11a140e30b7616e0e29116b254084f0c
PushpendraKPal <kumarpushp1992@gmail.com>
2024-01-25 11:25:36 +0530

    Inetial commit

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,14 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./components/expenseItem";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h2>Expense Items</h2>
-      <ExpenseItem />
-    </div>
-  );
-};
+function App() {
+  return <div>Expense Tracker</div>;
+}
 
 export default App;
--- expense_tracker/src/components/ExpenseChart.js
+++
@@ -1,8 +0,0 @@
-import Chart from "./chart/Chart";
-
-const ExpenseChart = (props) => {
-  const { filteredData } = props;
-  return <Chart filteredData={filteredData}></Chart>;
-};
-
-export default ExpenseChart;

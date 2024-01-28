288e33cc15e5f25d5952e4f9323214618838c7cd
PushpendraKPal <kumarpushp1992@gmail.com>
2024-01-28 13:32:28 +0530

    email verification

--- expense_tracker/src/pages/Welcome.jsx
+++ expense_tracker/src/pages/Welcome.jsx
@@ -1,10 +1,36 @@
 import { useNavigate } from "react-router-dom";
+import { AuthCxt } from "../contaxt/authContext/AuthContext";
 
 const Welcome = () => {
   const navigate = useNavigate();
+  const { token } = AuthCxt();
 
   const handleComplete = () => {
     navigate("/profileForm");
+  };
+
+  const handleVerifyMail = async () => {
+    try {
+      let response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyA0Q7-BgkEdY2M1RXH0W3uyrlRRqkrm5Gs",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: token,
+            returnSecureToken: true,
+          }),
+        }
+      );
+      let data = await response.json();
+      if (data.error) return alert(data.error.message);
+      else {
+        //alert("You have successfully registred!");
+        console.log(data);
+      }
+    } catch (err) {
+      //console.log(err, "Hello");
+    }
   };
   return (
     <div>
@@ -13,6 +39,7 @@ const Welcome = () => {
         Your profile is incomplete!{" "}
         <button onClick={handleComplete}>complete now!</button>
       </p>
+      <button onClick={handleVerifyMail}>Verify your email</button>
     </div>
   );
 };

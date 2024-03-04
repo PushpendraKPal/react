adabcf485761a90f7cb9d07386cbf8d075177f56
PushpendraKPal <kumarpushp1992@gmail.com>
2024-03-04 21:42:58 +0530

    jest testing

---
+++ jest_testing/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./component/Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <p>Learn React</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ jest_testing/src/component/Welcome.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import Welcome from "./Welcome";
+import userEvent from "@testing-library/user-event";
+
+test("Welcome test", () => {
+  render(<Welcome />);
+
+  const welcomeElement = screen.getByText("Welcome");
+  expect(welcomeElement).toBeInTheDocument();
+});
+
+test("Thanks message", () => {
+  render(<Welcome />);
+
+  const thanksElement = screen.getByText("Thanks again.");
+  expect(thanksElement).toBeInTheDocument();
+});
+
+test("Thanks message", () => {
+  render(<Welcome />);
+
+  const textElement = screen.getByText("Keep watching", { exact: false });
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Thanks message", () => {
+  render(<Welcome />);
+
+  const buttonElement = screen.getByRole("button");
+  userEvent.click(buttonElement);
+
+  const textElement = screen.queryByText("Thanks for joining Premium", {
+    exact: false,
+  });
+  expect(textElement).not.toBeInTheDocument();
+});
